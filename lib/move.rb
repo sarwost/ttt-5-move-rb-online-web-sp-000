02a8ce17289fe2e4b09f131141f5d9a237d9e83a
Sarah Wostenberg <swostenberg@gmail.com>
2020-12-14 14:59:40 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end 
 
-def move(board, index, value = "X")
-  board[index] = value 
+def move(array, index, value = "X")
+  array[index] = value 
 end 
